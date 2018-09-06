b06b22842d27f3b59cd6c0f255c05594471caab6
Safayat Arefin <abusafayatarefin92@gmail.com>
2018-09-06 13:16:45 +0600

    kjzhsdfjzsydgfj,zsdhbfvkzsj,vbjxchbvzsjvb

---
+++ JAVA/09-06-2018/CurrentTime/src/NumericTypeConversion.java
@@ -0,0 +1,9 @@
+
+public class NumericTypeConversion {
+
+    public static void main(String[] args) {
+        int x = 10;
+        long y = x;
+        System.out.println("X: " + x + ", Y: " + y);
+    }
+}
