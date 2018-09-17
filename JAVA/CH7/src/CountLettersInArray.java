9011a059446bc0efda3ee06dac3398280eb9d9e3
Safayat Arefin <abusafayatarefin92@gmail.com>
2018-09-17 13:33:31 +0600

    liaudlifuslckvjbaliurfgasilvzcxhvbzliurfgvilzuscvhzf

---
+++ JAVA/CH7/src/CountLettersInArray.java
@@ -0,0 +1,44 @@
+public class CountLettersInArray {
+  public static void main(String[] args) {
+   
+}
+  
+  public static char[] createArray(){
+  char[] chars = new char[100];
+  
+  for(int i = 0; i < chars.length; i++){
+  chars[i] = RandomCharacter.gettRandomLowerCaseLetter();
+  }
+  return chars;
+  }
+  
+  public static void displayArray(char[] chars){
+  for(int i = 0; i < chars.length; i++){
+  if((i + 1) % 20 == 0){
+      System.out.println(chars[i]);
+  }else{
+  System.out.println(chars[i] + " ");
+  }
+  }
+  }
+  
+  public static int[] countLetters(char[] chars){
+  int[] counts = new int[26];
+  
+  for(int i = 0; i < chars.length; i++){
+  counts[chars[i] - 'a']++;
+  }
+   return counts;
+  }
+  
+  public static void displayCounts(int[] counts){
+  for(int i = 0; i < counts.length; i++){
+  if((i + 1) % 10 == 0){
+      System.out.println(counts[i] + " " + (char)(i + 'a'));
+  }else{
+  System.out.println(counts[i] + " " + (char)(i + 'a') + " ");
+  }
+  }
+  }
+  
+}
