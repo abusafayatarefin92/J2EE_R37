2744cf7ff00b6588fd8bc7d820471ec36af297ba
Safayat Arefin <abusafayatarefin92@gmail.com>
2018-09-19 13:02:51 +0600

    ksuygdkjvhszkrufygkjzshcjzyfgsdjkvbjzsdfyv

---
+++ JAVA/CH9/src/WhoExecutesFirst.java
@@ -0,0 +1,15 @@
+
+public class WhoExecutesFirst {
+
+    public static void main(String[] args) {
+        System.out.println("I am from main method");
+    }
+
+    static {
+        System.out.println("I am from static block");
+    }
+
+    public WhoExecutesFirst() {
+        System.out.println("I am from constructor");
+    }
+}
