92db0c3b327978c67c0d5aab5e439ba7c361f202
Safayat_Arefin <safayatarefin@gmail.com>
2019-02-23 20:39:22 +0600

    userrepo

--- ArefinSpring/springsecuritymysql/src/main/java/com/springsecuritymysql/controller/RoleController.java
+++ ArefinSpring/springsecuritymysql/src/main/java/com/springsecuritymysql/controller/RoleController.java
@@ -19,23 +19,6 @@ public class RoleController {
     @Autowired
     private RoleRepo roleRepo;
 
-//    @GetMapping(value = "/role-save")
-//    public String saveRole(){
-//        Role role = new Role();
-//        role.setRoleName("SUPERADMIN");
-//        roleRepo.save(role);
-//
-//        Role role2 = new Role();
-//        role2.setRoleName("ADMIN");
-//        roleRepo.save(role2);
-//
-//        Role role3 = new Role();
-//        role3.setRoleName("USER");
-//        roleRepo.save(role3);
-//
-//        return "success";
-//    }
-
     @GetMapping(value = "add")
     public String addRoleView(Role role){
         return "role/add";
