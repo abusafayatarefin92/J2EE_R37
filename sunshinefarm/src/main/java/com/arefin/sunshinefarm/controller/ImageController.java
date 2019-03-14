5a2f25afd0429c7241973614d9b088eb346e00d3
Safayat Arefin <abusafayatarefin92@gmail.com>
2019-03-14 13:42:38 +0600

    3/14/2019morning

--- sunshinefarm/src/main/java/com/arefin/sunshinefarm/controller/ImageController.java
+++ sunshinefarm/src/main/java/com/arefin/sunshinefarm/controller/ImageController.java
@@ -1,9 +1,9 @@
 package com.arefin.sunshinefarm.controller;
 
-import com.arefin.sunshinefarm.entity.Role;
 import com.arefin.sunshinefarm.entity.User;
 import com.arefin.sunshinefarm.image.ImageOptimizer;
-import com.arefin.sunshinefarm.repo.*;
+import com.arefin.sunshinefarm.repo.RoleRepo;
+import com.arefin.sunshinefarm.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,40 +22,14 @@ import javax.validation.Valid;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashSet;
-import java.util.Set;
 
 @Controller
-public class CustomersController {
-    @Autowired
-    private CropsRepo cropsRepo;
-
-    @Autowired
-    RoleRepo roleRepo;
-
-    @Autowired
-    private CropsSummaryRepo cropsSummaryRepo;
-
-    @Autowired
-    private EmployeesRepo employeesRepo;
-
-    @Autowired
-    private EquipmentRepo equipmentRepo;
-
-    @Autowired
-    private ExpenseRepo expenseRepo;
-
-    @Autowired
-    private InsecticidesRepo insecticidesRepo;
-
-    @Autowired
-    private PesticidesRepo pesticidesRepo;
-
-    @Autowired
-    private SalesRepo salesRepo;
-
+public class ImageController {
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private RoleRepo roleRepo;
 
     @Autowired
     PasswordEncoder passwordEncoder;
@@ -65,60 +39,6 @@ public class CustomersController {
 
     @Autowired
     private ImageOptimizer imageOptimizer;
-
-    @GetMapping(value = "/crops-list")
-    public String cropsList(Model model){
-        model.addAttribute("crops", this.cropsRepo.findAll());
-        return "customers/crops-list";
-    }
-
-    @GetMapping(value = "/crops-summary-list")
-    public String cropsSummaryList(Model model){
-        model.addAttribute("cropssummary", this.cropsSummaryRepo.findAll());
-        return "customers/crops-summary-list";
-    }
-
-    @GetMapping(value = "/employees-list")
-    public String employeesList(Model model){
-        model.addAttribute("employees", this.employeesRepo.findAll());
-        return "customers/employees-list";
-    }
-
-    @GetMapping(value = "/equipment-list")
-    public String equipmentList(Model model){
-        model.addAttribute("equipment", this.equipmentRepo.findAll());
-        return "customers/equipment-list";
-    }
-
-    @GetMapping(value = "/expenses-list")
-    public String expensesList(Model model){
-        model.addAttribute("expenses", this.expenseRepo.findAll());
-        return "customers/expenses-list";
-    }
-
-    @GetMapping(value = "/insecticides-list")
-    public String insecticidesList(Model model){
-        model.addAttribute("insecticides", this.insecticidesRepo.findAll());
-        return "customers/insecticides-list";
-    }
-
-    @GetMapping(value = "/pesticides-list")
-    public String pesticidesList(Model model){
-        model.addAttribute("pesticides", this.pesticidesRepo.findAll());
-        return "customers/pesticides-list";
-    }
-
-    @GetMapping(value = "/sales-list")
-    public String salesList(Model model){
-        model.addAttribute("sales", this.salesRepo.findAll());
-        return "customers/sales-list";
-    }
-
-//    @GetMapping(value = "/user-list")
-//    public String userList(Model model){
-//        model.addAttribute("user", this.userRepo.findAll());
-//        return "customers/user-list";
-//    }
 
     @GetMapping(value = "/user-list")
     public ModelAndView getList() {
