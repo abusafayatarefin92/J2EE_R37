51623d6991eb6a4682604c09a62d3224520268dd
Safayat_Arefin <safayatarefin@gmail.com>
2019-03-30 13:03:32 +0600

    3/30/2019Morning

---
+++ ArefinAndriod/ContextMenu2/app/src/main/java/com/example/contextmenu/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.contextmenu;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    String[] names = {"Arefin", "Zakir", "Ariful"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listview);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+
+    }
+}
