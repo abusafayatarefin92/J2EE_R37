709650367b06966fc72b672f62b8b82295296b0a
Safayat_Arefin <safayatarefin@gmail.com>
2019-04-11 08:19:16 +0600

    4/11/2019Morning

---
+++ ArefinAndriod/GSMCloudMessagingExample/app/src/main/java/com/arefin/gsmcloudmessagingexample/MyFireBaseMessagingService.java
@@ -0,0 +1,26 @@
+package com.arefin.gsmcloudmessagingexample;
+
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFireBaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+    }
+
+    public void showNotification(String title, String message){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "myNotification")
+        .setContentTitle(title)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setAutoCancel(true)
+                .setContentText(message);
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+        manager.notify(999,builder.build());
+    }
+}
